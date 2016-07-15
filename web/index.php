1bd8477f26107c0c3b81fd70be7551438d7c66fd
JunkiMiura <gachame_pudding@yahoo.co.jp>
2016-07-15 21:45:16 +0900

    shirangana

--- web/index.php
+++ web/index.php
@@ -1,41 +1,42 @@
 <?php
-
-require('../vendor/autoload.php');
+require ('../vendor/autoload.php');
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-$app = new Silex\Application();
-$bot = new CU\LineBot();
+$app = new Silex\Application ();
+$bot = new CU\LineBot ();
 
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-    'monolog.logfile' => 'php://stderr',
-));
+$app->register ( new Silex\Provider\MonologServiceProvider (), array (
+		'monolog.logfile' => 'php://stderr'
+) );
 
-$app->before(function (Request $request) use($bot) {
-    // Signature validation
-    $request_body = $request->getContent();
-    $signature = $request->headers->get('X-LINE-CHANNELSIGNATURE');
-    if (!$bot->isValid($signature, $request_body)) {
-        return new Response('Signature validation failed.', 400);
-    }
-});
+$app->before ( function (Request $request) use ($bot) {
+	// Signature validation
+	$request_body = $request->getContent ();
+	$signature = $request->headers->get ( 'X-LINE-CHANNELSIGNATURE' );
+	if (! $bot->isValid ( $signature, $request_body )) {
+		return new Response ( 'Signature validation failed.', 400 );
+	}
+} );
 
-$app->post('/callback', function (Request $request) use ($app, $bot) {
-    // Let's hack from here!
-    $body = json_decode($request->getContent(), true);
+$app->post ( '/callback', function (Request $request) use ($app, $bot) {
+	// Let's hack from here!
+	$body = json_decode ( $request->getContent (), true );
 
-    foreach ($body['result'] as $obj) {
-        $app['monolog']->addInfo(sprintf('obj: %s', json_encode($obj)));
-        $from = $obj['content']['from'];
-        $content = $obj['content'];
+	foreach ( $body ['result'] as $obj ) {
+		$app ['monolog']->addInfo ( sprintf ( 'obj: %s', json_encode ( $obj ) ) );
+		$from = $obj ['content'] ['from'];
+		$content = $obj ['content'];
 
-        if ($content['text']) {
-            $bot->sendText($from, sprintf("「%s」アホちゃう", $content['text']));
-        }
-    }
+		if ($content ['text'] == "しらんがな") {
+			$bot->sendText ( $from, printf ("(´･ω･｀)しらんがな") );
+		} else {
+			$bot->sendText ( $from, sprintf ( "「%s」じゃねーよ！！", $content ['text'] ) );
+		}
+	}
 
-    return 0;
-});
+	return 0;
+} );
 
-$app->run();
+$app->run ();
